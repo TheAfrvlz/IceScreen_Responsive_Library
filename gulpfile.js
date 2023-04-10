7677b20e263b01dfbd2e4411cd2106d6f807d13c
Chuy <Jvelez13Soto@outlook.com>
2023-04-09 18:18:51 -0600

    Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, series } = require("gulp");
 const minifyCss = require("gulp-clean-css");
 const sourceMap = require("gulp-sourcemaps");
-const concat = require('gulp-concat');
+const concat = require("gulp-concat");
 
 const Styles = [
   "./Components/Imports.css",
@@ -22,26 +22,80 @@ const Fonts_Col = [];
 const IceScreenCss_Col = [];
 const IceScreenComponents_Col = [];
 
-
 const PrincipalBundle = () => {
   return src("./Components/*.css")
     .pipe(sourceMap.init())
     .pipe(minifyCss())
-    .pipe(concat('Production.css'))
+    .pipe(concat("IceScreen_bun_min_css.css"))
     .pipe(sourceMap.write())
     .pipe(dest("./Out/"));
 };
 
-
-const devWatch = (Collection2Watch,Bundle) => {
-  watch(Collection2Watch,PrincipalBundlendle);
+const devWatch = (Collection2Watch, Bundle) => {
+  watch(Collection2Watch, PrincipalBundlendle);
+};
+const Responsive = () => {
+  return src(Responsive_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Responsive_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
 };
 
-
+const MobileCSS = () => {
+  return src(MobileCSS_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Mobile_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
+const DesktopCSS = () => {
+  return src(Desktop_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Desktop_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
+const Colors = () => {
+  return src(Colors_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Colors_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
+const Fonts = () => {
+  return src(Fonts_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Fonts_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
+const IceScreenCss = () => {
+  return src(IceScreenCss_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_bun_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
+const IceScreenComponents = () => {
+  return src(IceScreenComponents_Col)
+    .pipe(sourceMap.init())
+    .pipe(minifyCss())
+    .pipe(concat("IceScreen_Components_min_css.css"))
+    .pipe(sourceMap.write())
+    .pipe(dest("./Out/"));
+};
 //bundles for multiple purposes
 // Includes all Screen Sizes
-exports.Responsive = Responsive;
+exports.PrincipalBundle = PrincipalBundle;
 
+exports.Responsive = Responsive;
 //Include Screen Sizes for Mobiles Only
 exports.MobileCSS = MobileCSS;
 
@@ -62,3 +116,12 @@ exports.IceScreenComponents = IceScreenComponents;
 
 //Include Watch
 exports.devWatch = devWatch;
+
+exports.build = series(
+  Responsive,
+  MobileCSS,
+  DesktopCSS,
+  Colors,
+  IceScreenCss,
+  IceScreenComponents
+);
