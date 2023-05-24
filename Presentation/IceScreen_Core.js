625ac78e9080638224a7c703968f08d2ae13a22f
Chuy <Jvelez13Soto@outlook.com>
2023-05-23 23:13:20 -0600

    NodeModules Deletes & Presentation

---
+++ Presentation/IceScreen_Core.js
@@ -0,0 +1,160 @@
+
+const ColorsVar = [
+  /* Escala de grises */
+  "#000000",
+  "#111111",
+  "#333333",
+  "#666666",
+  "#999999",
+  "#cccccc",
+  "#e6e6e6",
+  "#f2f2f2",
+  "#ffffff",
+
+  "#e6f4ff",
+  "#c3e0ff",
+  "#a2ceff",
+  "#80baff",
+  "#5d9bff",
+  "#397dff",
+  "#1652ff",
+  "#004cff",
+  "#0039cc",
+
+  /* Tonos de rojo */
+  "#ffebe6",
+  "#ffc2b3",
+  "#ff9980",
+  "#ff704d",
+  "#ff471a",
+  "#d92700",
+  "#a60000",
+  "#7a0000",
+  "#4d0000",
+
+  /* Tonos de verde */
+  "#e3f9e5",
+  "#c1e7c4",
+  "#a0d5a4",
+  "#7ec479",
+  "#5cb15a",
+  "#3a913c",
+  "#176e2f",
+  "#0f5723",
+  "#0d4218",
+
+  /* Tonos de amarillo */
+  "#fffce0",
+  "#fff4b3",
+  "#ffe680",
+  "#ffd24d",
+  "#ffc200",
+  "#F6C90E",
+  "#d9a400",
+  "#a66b00",
+  "#804c00",
+  /* Tonos de cyan */
+  "#e0ffff",
+  "#b3ffff",
+  "#80ffff",
+  "#4dffff",
+  "#1affff",
+  "#00e6e6",
+  "#00b3b3",
+  "#008080",
+  "#004d4d",
+
+  /* Tonos de Sky */
+  "#e0f2ff",
+  "#b3e6ff",
+  "#80d4ff",
+  "#4dc2ff",
+  "#1aafff",
+  "#00a3cc",
+  "#008fb3",
+  "#006680",
+  "#003d4d",
+
+  /* Tonos de amber */
+  "#fff4e0",
+  "#ffe0b3",
+  "#ffce80",
+  "#ffba4d",
+  "#ffa11a",
+  "#cc8a00",
+  "#997a00",
+  "#664c00",
+  "#332600",
+
+  /* Tonos de violeta */
+  "#f2e0ff",
+  "#e6b3ff",
+  "#d480ff",
+  "#c24dff",
+  "#a11aff",
+  "#8a00cc",
+  "#6600a3",
+  "#4c0080",
+  "#33004d",
+
+  /* Tonos de fuchsia */
+  "#ffe0f2",
+  "#ffb3e6",
+  "#ff80d4",
+  "#ff4dc2",
+  "#ff1aa1",
+  "#cc008a",
+  "#990066",
+  "#66004c",
+  "#330026",
+
+  /* Tonos de rosa */
+  "#fff2f2",
+  "#ffe6e6",
+  "#ffd4d4",
+  "#ffc2c2",
+  "#ffafaf",
+  "#cc8a8a",
+  "#996666",
+  "#663333",
+  "#330000",
+
+  /* Tonos de morado */
+  "#f4e6ff",
+  "#e0c3ff",
+  "#cea2ff",
+  "#ba80ff",
+  "#9b5dff",
+  "#7d39ff",
+  "#5216ff",
+  "#4c00ff",
+  "#3900cc",
+
+  /* Tonos de naranja */
+  "#fff4e0",
+  "#ffe0c3",
+  "#ffcea2",
+  "#ffba80",
+  "#ff9b5d",
+  "#ff7d39",
+  "#ff5216",
+  "#ff4c00",
+  "#cc3900",
+];
+
+const CColors = document.querySelector('.ColorsContainer');
+const CFonts = document.querySelector('.FontssContainer');
+const CComponents = document.querySelector('.ComponentsContainer');
+
+
+const Color = (Color) => {
+    return `<div class="Color" style="background-color:${Color};"></div>
+`;
+}
+
+
+ColorsVar.forEach(Col => {
+    CColors.innerHTML += Color(Col);
+});
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const { src, dest, watch, series } = require("gulp");
 const minifyCss = require("gulp-clean-css");
 const sourceMap = require("gulp-sourcemaps");
-const concat = require("gulp-concat");
+const concatCss = require("gulp-concat");
+const cssMinify = require("gulp-css-minify");
+const cssnano = require("gulp-cssnano");
 
 const Styles = [
   "./Components/Imports.css",
@@ -14,20 +16,46 @@ const Styles = [
   "./Components/Works.css",
 ];
 
-const Responsive_Col = [];
-const MobileCSS_Col = [];
-const Desktop_Col = [];
-const Colors_Col = [];
+const Responsive_Col = [
+  "./Components/xxG.css",
+  "./Components/xG.css",
+  "./Components/G.css",
+  "./Components/M.css",
+  "./Components/S.css",
+  "./Components/xS.css",
+  "./Components/xxS.css",
+];
+const MobileCSS_Col = [
+  "./Components/S.css",
+  "./Components/xS.css",
+  "./Components/xxS.css",
+];
+const Desktop_Col = [
+  "./Components/xxG.css",
+  "./Components/xG.css",
+  "./Components/G.css",
+  "./Components/M.css",
+];
+const Colors_Col = [
+  "./Components/Colors.css",
+  "./Components/Gradients.css",
+  "./Components/PalletColors.css",
+];
 const Fonts_Col = [];
-const IceScreenCss_Col = [];
+const IceScreenCss_Col = [
+  "./Components/Navbar.css",
+  "./Components/Card.css",
+   "./Components/Breadcum.css",
+   "./Components/Link.css",
+  "./Components/.css",
+   "./Components/xxS.css"
+];
 const IceScreenComponents_Col = [];
 
 const PrincipalBundle = () => {
   return src("./Components/*.css")
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_bun_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Grl.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 
@@ -36,61 +64,90 @@ const devWatch = (Collection2Watch, Bundle) => {
 };
 const Responsive = () => {
   return src(Responsive_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Responsive_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Rsp.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 
 const MobileCSS = () => {
   return src(MobileCSS_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Mobile_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Mob.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 const DesktopCSS = () => {
   return src(Desktop_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Desktop_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Dsk.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 const Colors = () => {
   return src(Colors_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Colors_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Clr.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 const Fonts = () => {
   return src(Fonts_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Fonts_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_fts.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 const IceScreenCss = () => {
   return src(IceScreenCss_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_bun_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Grl.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
 const IceScreenComponents = () => {
   return src(IceScreenComponents_Col)
-    .pipe(sourceMap.init())
-    .pipe(minifyCss())
-    .pipe(concat("IceScreen_Components_min_css.css"))
-    .pipe(sourceMap.write())
+    .pipe(concatCss("IceScreen_Com.css"))
+    .pipe(cssnano())
     .pipe(dest("./Out/"));
 };
+
+const bundleHTML = () => {
+  return src("index.html")
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[0]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[1]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[2]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[3]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[4]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[5]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[6]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[7]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[8]}" />`, "")
+    )
+    .pipe(
+      replace(`<link rel="stylesheet" href="./src/Styles/${Styles[9]}" />`, "")
+    )
+    .pipe(
+      replace(
+        `<link rel="stylesheet" href="./src/Styles/${Styles[10]}" />`,
+        '<link rel="stylesheet" href="bundle.css"/>'
+      )
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("./Production/"));
+};
+
 //bundles for multiple purposes
 // Includes all Screen Sizes
 exports.PrincipalBundle = PrincipalBundle;
@@ -116,6 +173,8 @@ exports.IceScreenComponents = IceScreenComponents;
 
 //Include Watch
 exports.devWatch = devWatch;
+
+exports.bundleHTML = bundleHTML;
 
 exports.build = series(
   Responsive,
